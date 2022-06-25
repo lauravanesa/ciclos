c084d98acafb7e2e06e7cf6dbe9f8b1b3f6d3da0
laura vanesa <castro.arce@outllok.es>
2022-06-25 08:49:21 -0500

    numeros de dos en dos

---
+++ src/NumerosDeDosEnDos.java
@@ -0,0 +1,9 @@
+public class NumerosDeDosEnDos {
+    public static void main (String[]arg){
+        //imprimir 10 numeros de 2 en 2. empezando
+        for (int i = 2; i <=20 ; i= i +2){
+            System.out.println("numeros" + i);
+
+        }
+    }
+}
