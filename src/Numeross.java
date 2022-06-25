b65c685f7966e56342fb6c8a87abdda34d0f9871
laura vanesa <castro.arce@outllok.es>
2022-06-25 09:12:43 -0500

    cambio numero

---
+++ src/Numeross.java
@@ -0,0 +1,14 @@
+public class Numeross {
+    public static void main (String[]arg){
+        //imprimir 5 numeros
+        for (int i = 0 ; i <= 2; i++){
+            System.out.println("numero"+1);
+            System.out.println("Numero"+3);
+
+
+
+
+
+        }
+    }
+}
