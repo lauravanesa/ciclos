6735d3c78a9e1ae676dc500dcd284c4c377df6a6
laura vanesa <castro.arce@outllok.es>
2022-06-25 08:37:47 -0500

    este es un cambio a los numeros

--- src/EjemplosCiclo.java
+++ src/EjemplosCiclo.java
@@ -1,5 +1,11 @@
+import javax.swing.*;
+
 public class EjemplosCiclo {
     public static void main (String[] args){
+        for (int i = 0; i < 3; i ++){
+            JOptionPane.showMessageDialog(null,"Hola Mundo");
+
+        }
 
     }
 }
---
+++ src/Numeros.java
@@ -0,0 +1,13 @@
+public class Numeros {
+    public static void main (String[]arg){
+        //ejemplo escribir los numeros del 1 al 100
+        for (int i = 1; i <= 100; i ++){
+            System.out.println("numero:"+i);
+
+
+
+        }
+
+
+    }
+}
